eeb8dd9e392db56853376eb779183098972c6c3d
alpt <alpt@mail.ru>
2017-02-16 10:48:40 +0900

    160217

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=nsign',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ykt94_nsign',
+    'username' => '045959031_nsign',
+    'password' => '786421',
     'charset' => 'utf8',
 ];
